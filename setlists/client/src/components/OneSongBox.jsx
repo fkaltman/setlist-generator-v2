1f19beacb78e79672670c322718aaad1574e09c3
Francine Altman <francinekline@gmail.com>
2026-03-09 18:35:10 -0400

    feat: add password protection for deleting songs

--- setlists/client/src/components/OneSongBox.jsx
+++ setlists/client/src/components/OneSongBox.jsx
@@ -1,34 +1,62 @@
-import React from 'react'
-import Swal from 'sweetalert2';
+import React from "react";
+import Swal from "sweetalert2";
 
 export default function OneSongBox(props) {
   return (
     <>
       <div className="song-text">{props.song.name}</div>
       <div className="abbrev-text">{props.song.abbreviation}</div>
-      <div className="song-length">{props.song.length}</div><br />
+      <div className="song-length">{props.song.length}</div>
+      <br />
       {/* Edit songs button */}
-      <div><button className="song-edit-button" onClick={() => {
-        props.setFormData(props.song)
-        props.setEdit(props.song.id)
-      }}>Edit</button></div>
+      <div>
+        <button
+          className="song-edit-button"
+          onClick={() => {
+            props.setFormData(props.song);
+            props.setEdit(props.song.id);
+          }}
+        >
+          Edit
+        </button>
+      </div>
       {/* Delete Songs button */}
-      <div><button className="song-delete-button" onClick={() => {
-        // Delete warning modal from SweetAlert2 below
-        Swal.fire({
-          title: 'Are you sure?',
-          text: "You won't be able to reverse this!",
-          type: 'warning',
-          showCancelButton: true,
-          confirmButtonColor: '#81b878',
-          cancelButtonColor: '#eabfbf',
-          confirmButtonText: 'Yes, delete it!'
-        }).then((result) => {
-          if (result.value) {
-            props.removeSong(props.song.id)
-          }
-        })
-      }}>Delete</button></div>
+      <div>
+        <button
+          className="song-delete-button"
+          onClick={() => {
+            // Delete warning modal with password protection
+            Swal.fire({
+              title: "Delete Song?",
+              html: `
+            <p>You won't be able to reverse this!</p>
+            <input type="password" id="delete-password" class="swal2-input" placeholder="Enter password to delete">
+          `,
+              icon: "warning",
+              showCancelButton: true,
+              confirmButtonColor: "#81b878",
+              cancelButtonColor: "#eabfbf",
+              confirmButtonText: "Yes, delete it!",
+              preConfirm: () => {
+                const password =
+                  document.getElementById("delete-password").value;
+                if (password !== "safecamp") {
+                  Swal.showValidationMessage("Incorrect password");
+                  return false;
+                }
+                return true;
+              },
+            }).then((result) => {
+              if (result.isConfirmed) {
+                props.removeSong(props.song.id);
+                Swal.fire("Deleted!", "The song has been deleted.", "success");
+              }
+            });
+          }}
+        >
+          Delete
+        </button>
+      </div>
     </>
-  )
+  );
 }
