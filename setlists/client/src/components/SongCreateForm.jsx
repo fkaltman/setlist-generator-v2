3d93a20609bd2062132f780cdb4d8388bf3c9dc6
Francine Altman <francinekline@gmail.com>
2026-03-10 14:04:54 -0400

    fix: lift form state up to resolve form clearing issue

--- setlists/client/src/components/SongCreateForm.jsx
+++ setlists/client/src/components/SongCreateForm.jsx
@@ -1,71 +1,47 @@
-import React from 'react';
+import React from "react";
 
 export default class SongCreateForm extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      formData: {
-        name: "",
-        abbreviation: "",
-        length: ""
-      }
-    }
-  }
-
-  // Identical to the other handleChange
-  handleChange = (e) => {
-    // Deconstructs e.target.name & e.target.value
-    // (pulls them out and makes them thier own variables)
-    const { name, value } = e.target
-    this.setState((prevState) => ({
-      formData: {
-        // Keep the previous data and append the new data to the array
-        ...prevState.formData,
-        // below line allows a key to become a variable
-        [name]: value
-      }
-    }))
-  }
-
-
   render() {
     return (
       <div className="add-song-form">
         <h2 className="add-song-header">Add a song to the database...</h2>
         <div className="new-song-inputs">
-          <form onSubmit={async (e) => {
-            e.preventDefault()
-            // songCreateHandleSubmit is being passed down from App.js
-            // and we are giving it the form data
-            await this.props.songCreateHandleSubmit(this.state.formData)
-          }}>
-            <input className="song-input"
+          <form
+            onSubmit={async (e) => {
+              e.preventDefault();
+              await this.props.songCreateHandleSubmit(this.props.formData);
+            }}
+          >
+            <input
+              className="song-input"
               type="text"
               placeholder="Name"
               name="name"
-              onChange={this.handleChange}
+              onChange={this.props.handleChange}
               // formData could be anything
-              value={this.state.formData.name}
-            /> <br />
-            <input className="abbrev-input"
+              value={this.props.formData.name}
+            />{" "}
+            <br />
+            <input
+              className="abbrev-input"
               type="text"
               placeholder="Abbreviation"
               name="abbreviation"
-              onChange={this.handleChange}
-              value={this.state.formData.abbreviation}
+              onChange={this.props.handleChange}
+              value={this.props.formData.abbreviation}
             />
-            <input className="length-input"
+            <input
+              className="length-input"
               type="string"
               placeholder="Length"
               name="length"
-              onChange={this.handleChange}
-              value={this.state.formData.city}
+              onChange={this.props.handleChange}
+              value={this.props.formData.length}
             />
             <button className="save-song-button">Save</button>
           </form>
         </div>
       </div>
-    )
+    );
   }
 }
-
--- setlists/client/src/components/SongsMasterList.jsx
+++ setlists/client/src/components/SongsMasterList.jsx
@@ -1,69 +1,53 @@
-import React from 'react';
-import OneSongBox from './OneSongBox';
-import EditSongForm from './EditSongForm';
-import SongCreateForm from './SongCreateForm';
-import Segno from '../assets/segno.png';
+import React from "react";
+import OneSongBox from "./OneSongBox";
+import EditSongForm from "./EditSongForm";
+import SongCreateForm from "./SongCreateForm";
+import Segno from "../assets/segno.png";
 
 export default class SongsMasterList extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       isEdit: null,
-      formData: {
-        name: "",
-        abbreviation: "",
-        length: ""
-      }
-    }
+    };
   }
 
   resetEdit = () => {
     this.setState({
-      isEdit: null
-    })
-  }
+      isEdit: null,
+    });
+  };
 
   setEdit = (id) => {
     this.setState({
-      isEdit: id
-    })
-  }
+      isEdit: id,
+    });
+  };
 
   setFormData = (song) => {
-    this.setState({
-      // set the formData to what the song was pre-edit
-      formData: song
-    })
-  }
-
-  // Identical to the other handleChange
-  handleChange = (e) => {
-    // Deconstructs e.target.name & e.target.value
-    // (pulls them out and makes them their own variables)
-    const { name, value } = e.target
-    this.setState((prevState) => ({
-      formData: {
-        // Keep the previous data and append the new data to the array
-        ...prevState.formData,
-        // below line allows a key to become a variable
-        [name]: value
-      }
-    }))
-  }
+    // Call the parent's setFormData instead
+    this.props.setFormData(song.id);
+  };
 
   render() {
     return (
       <div className="masterlist-and-add-a-song">
-        <img className="segno-image" src={Segno} alt="home button" onClick={this.props.segnoHandleSubmit} />
+        <img
+          className="segno-image"
+          src={Segno}
+          alt="home button"
+          onClick={this.props.segnoHandleSubmit}
+        />
         <h3 className="master-songlist">Master Songlist Library</h3>
-        <h4 className="master-songlist-subtitle">Scroll to view, edit and delete songs below...</h4>
+        <h4 className="master-songlist-subtitle">
+          Scroll to view, edit and delete songs below...
+        </h4>
         <div className="all-songs-map">
           {this.props.songs.map((song) => (
             // the line below doesn't really add functionality, the key
             // is required by react and the className is just for styling
             <div key={song.id} className="all-songs-one-box">
-              {this.state.isEdit === song.id
-                ?
+              {this.state.isEdit === song.id ? (
                 <EditSongForm
                   // Below are all props
                   formData={this.state.formData}
@@ -71,25 +55,22 @@ export default class SongsMasterList extends React.Component {
                   songUpdateHandleSubmit={this.props.songUpdateHandleSubmit}
                   resetEdit={this.resetEdit}
                 />
-                :
+              ) : (
                 <>
                   {/* the first song below is used in OneSongBox.jsx */}
-                  < OneSongBox
+                  <OneSongBox
                     {...this.props}
                     song={song}
                     setEdit={this.setEdit}
                     setFormData={this.setFormData}
                   />
                 </>
-              }
-            </div >
-          ))
-          }
+              )}
+            </div>
+          ))}
         </div>
-        <SongCreateForm
-          {...this.props}
-        />
+        <SongCreateForm {...this.props} />
       </div>
-    )
+    );
   }
 }
