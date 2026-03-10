60cc8f38bdf91912814e4da423a15a3d276166de
Francine Altman <francinekline@gmail.com>
2026-03-10 10:52:32 -0400

    fix: resolve build and lint errors

--- setlists/client/src/App.js
+++ setlists/client/src/App.js
@@ -1,25 +1,30 @@
-import React from 'react';
-import './App.css';
-import { fetchSongs, createSong, deleteSong, editSong } from './services/api-helper';
-import Home from './components/Home';
-import SongsMasterList from './components/SongsMasterList';
-import GenerateSetlist from './components/GenerateSetlist';
+import React from "react";
+import "./App.css";
+import {
+  fetchSongs,
+  createSong,
+  deleteSong,
+  editSong,
+} from "./services/api-helper";
+import Home from "./components/Home";
+import SongsMasterList from "./components/SongsMasterList";
+import GenerateSetlist from "./components/GenerateSetlist";
 import { Route, Switch, withRouter } from "react-router-dom";
 
 class App extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       songs: [],
       setlists: [],
       formData: {
         name: "",
         abbreviation: "",
-        length: ""
+        length: "",
       },
       // below line holds the id of the song while the user updates it
-      updatingId: null
-    }
+      updatingId: null,
+    };
   }
 
   songCreateHandleSubmit = async (songData) => {
@@ -37,22 +42,24 @@ class App extends React.Component {
       formData: {
         name: "",
         abbreviation: "",
-        length: ""
-      }
-    })
-  }
+        length: "",
+      },
+    });
+  };
 
   segnoHandleSubmit = () => {
-    this.props.history.push("/")
-  }
+    this.props.history.push("/");
+  };
 
   songUpdateHandleSubmit = async (songData) => {
     const { id, ...data } = songData;
-    const updateSong = await editSong(id, data)
+    const updateSong = await editSong(id, data);
     this.setState((prevState) => ({
-      songs: prevState.songs.map(song => id === song.id ? updateSong : song)
-    }))
-  }
+      songs: prevState.songs.map((song) =>
+        id === song.id ? updateSong : song,
+      ),
+    }));
+  };
 
   removeSong = async (id) => {
     await deleteSong(id);
@@ -60,82 +67,89 @@ class App extends React.Component {
       // Below filters through the songs array and compairs if the one
       // it is given is equal to all of the ids in the array
       // AND sets it in state
-      songs: prevState.songs.filter(song => id !== song.id)
-    }))
-  }
+      songs: prevState.songs.filter((song) => id !== song.id),
+    }));
+  };
 
   removeGeneratedSong = async (id) => {
     this.setState((prevState) => ({
       // Below filters through the songs array and compairs if the one
       // it is given is equal to all of the ids in the array
       // AND sets it in state
-      songs: prevState.songs.filter(song => id !== song.id)
-    }))
-  }
+      songs: prevState.songs.filter((song) => id !== song.id),
+    }));
+  };
 
   // Identical to the other handleChange
   handleChange = (e) => {
     // Deconstructs e.target.name & e.target.value
     // (pulls them out and makes them their own variables)
-    const { name, value } = e.target
+    const { name, value } = e.target;
     this.setState((prevState) => ({
       formData: {
         // Keep the previous data and append the new data to the array
         ...prevState.formData,
         // below line allows a key to become a variable
-        [name]: value
-      }
-    }))
-  }
+        [name]: value,
+      },
+    }));
+  };
 
   componentDidMount = async () => {
     const currentSongs = await fetchSongs();
     this.setState({
-      songs: currentSongs
-    })
-  }
+      songs: currentSongs,
+    });
+  };
 
   // Set the form data to the selected song and store its id
   setFormData = (id) => {
-    const currentSong = this.state.songs.find(song => song.id === id)
+    const currentSong = this.state.songs.find((song) => song.id === id);
     this.setState({
       formData: currentSong,
-      updatingId: id
-    })
-  }
+      updatingId: id,
+    });
+  };
 
   render() {
     return (
       <div>
         <Switch>
-          <Route exact path='/' component={Home} />
-          <Route exact path='/generate-setlist' render={() => (
-            <GenerateSetlist
-              formData={this.state.formData}
-              segnoHandleSubmit={this.segnoHandleSubmit}
-            />)} />
+          <Route exact path="/" component={Home} />
+          <Route
+            exact
+            path="/generate-setlist"
+            render={() => (
+              <GenerateSetlist
+                formData={this.state.formData}
+                segnoHandleSubmit={this.segnoHandleSubmit}
+              />
+            )}
+          />
           {/* <Route path='/setlist-archives' component={SetlistArchives} /> */}
-          <Route path='/songs-masterlist' render={() => (
-            <SongsMasterList
-              // Below two lines are passing data, note the presence of "state"
-              // compaired to the methods below them
-              songs={this.state.songs}
-              formData={this.state.formData}
-              // Below is how we pass in a method that is defined in this component
-              removeSong={this.removeSong}
-              handleChange={this.handleChange}
-              // Below is passing updateSong to SongsMasterList
-              setFormData={this.setFormData}
-              songUpdateHandleSubmit={this.songUpdateHandleSubmit}
-              segnoHandleSubmit={this.segnoHandleSubmit}
-              songCreateHandleSubmit={this.songCreateHandleSubmit}
-            />)} />
+          <Route
+            path="/songs-masterlist"
+            render={() => (
+              <SongsMasterList
+                // Below two lines are passing data, note the presence of "state"
+                // compaired to the methods below them
+                songs={this.state.songs}
+                formData={this.state.formData}
+                // Below is how we pass in a method that is defined in this component
+                removeSong={this.removeSong}
+                handleChange={this.handleChange}
+                // Below is passing updateSong to SongsMasterList
+                setFormData={this.setFormData}
+                songUpdateHandleSubmit={this.songUpdateHandleSubmit}
+                segnoHandleSubmit={this.segnoHandleSubmit}
+                songCreateHandleSubmit={this.songCreateHandleSubmit}
+              />
+            )}
+          />
         </Switch>
       </div>
-    )
+    );
   }
 }
 
 export default withRouter(App);
-
-
--- setlists/client/src/components/Home.jsx
+++ setlists/client/src/components/Home.jsx
@@ -1,24 +1,30 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import Typewriter from '../assets/typewriter_nw_700.png';
-
+import React from "react";
+import { Link } from "react-router-dom";
+import Typewriter from "../assets/typewriter_nw_700.png";
 
 export default function Home() {
   return (
     <div className="homepage">
       <p className="line-1 anim-typewriter">Setlist Generator</p>
       <div className="home-links">
-        <Link to='/generate-setlist'>
-          <button className="home-generate-a-list-button" onClick={() => { }}>Create New Setlists</button></Link>
+        <Link to="/generate-setlist">
+          <button className="home-generate-a-list-button" onClick={() => {}}>
+            Create New Setlists
+          </button>
+        </Link>
         <br />
         {/* <Link to='/setlist-archives'>
           <button className="home-go-to-archives-button">Setlist Archives</button>
         </Link><br /> */}
-        <Link to='/songs-masterlist'>
-          <button className="home-go-songs-masterlist-button">Songs Masterlist</button>
+        <Link to="/songs-masterlist">
+          <button className="home-go-songs-masterlist-button">
+            Songs Masterlist
+          </button>
         </Link>
       </div>
-      <div className="typewriter"><img src={Typewriter} alt="typewriter" /></div>
+      <div className="typewriter">
+        <img src={Typewriter} alt="typewriter" />
+      </div>
     </div>
-  )
+  );
 }
